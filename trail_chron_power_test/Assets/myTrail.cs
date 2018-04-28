429316d6af066d05f1413308a70fd41106ae3cb0
ryu <abukame@gmail.com>
2018-04-28 11:48:40 +0900

    初期addForceつけたのと、いろいろforceの数値を調整した

--- trail_chron_power_test/Assets/myTrail.cs
+++ trail_chron_power_test/Assets/myTrail.cs
@@ -11,7 +11,13 @@ public class myTrail : MonoBehaviour {
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 		go_list = GameObject.FindGameObjectsWithTag("Player");
-		transform.position = Random.onUnitSphere * 40;
+		transform.position = Random.onUnitSphere * 40f;
+
+		//球表面の接線方向へのパワーだとおもう
+		//そして、ここ、Impulusつけないと、全然結果が違う。
+		//Impulusなるほど。dtがデカすぎるなるほどｗｗｗ
+		rb.AddForce(Vector3.Cross(transform.position,Random.onUnitSphere),ForceMode.Impulse);
+
 	}
 	
 	// Update is called once per frame
@@ -25,9 +31,12 @@ public class myTrail : MonoBehaviour {
 
 			var diff = go.transform.position - transform.position;
 			var len = diff.sqrMagnitude;
-			var force = diff/len;
 
-			rb.AddForce(force);
+			//normalized忘れてた。lenで割ってるからねいるよね
+			var force = diff.normalized/len;
+
+			//100とかマジックナンバー。適当
+			rb.AddForce(force * 100f);
 		}
 
 		rb.AddForce(-transform.position);
