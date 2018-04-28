ec0fd909d4c7398ff07de2cd2e334116ce99f7aa
ryu <abukame@gmail.com>
2018-04-28 11:37:19 +0900

    クーロン力？っぽい動きにした

---
+++ trail_chron_power_test/Assets/myTrail.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myTrail : MonoBehaviour {
+
+	Rigidbody rb;
+	GameObject[] go_list;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		go_list = GameObject.FindGameObjectsWithTag("Player");
+		transform.position = Random.onUnitSphere * 40;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		foreach(GameObject go in go_list){
+
+			if(this.gameObject.GetInstanceID() == go.GetInstanceID()){
+				continue;
+			}
+
+			var diff = go.transform.position - transform.position;
+			var len = diff.sqrMagnitude;
+			var force = diff/len;
+
+			rb.AddForce(force);
+		}
+
+		rb.AddForce(-transform.position);
+
+	}
+}
