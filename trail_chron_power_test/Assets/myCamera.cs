40134126264b6364e03dd855c4a5e9b3bbbba4e6
ryu <abukame@gmail.com>
2018-04-28 11:54:53 +0900

    カメラを回してみてた。V3 = Q * V3で位置更新。忘れてるなぁ

---
+++ trail_chron_power_test/Assets/myCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myCamera : MonoBehaviour {
+
+	//初期位置
+	Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Quaternion.Euler(0,Time.time * 10,0) * position;
+		transform.LookAt(Vector3.zero);
+	}
+}
